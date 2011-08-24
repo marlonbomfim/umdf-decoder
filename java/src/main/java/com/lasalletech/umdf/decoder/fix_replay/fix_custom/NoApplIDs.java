7a7436306cfd6b22b9ded88bb9e12891aaa749e5
wes <wes@lasalletech.com>
2011-08-24 20:11:04 +0000

    - Renamed ApplChannelID field to proper name 'RefApplID' - Added sequence order for NoApplIDs

--- java/src/main/java/com/lasalletech/umdf/decoder/fix_replay/fix_custom/NoApplIDs.java
+++ java/src/main/java/com/lasalletech/umdf/decoder/fix_replay/fix_custom/NoApplIDs.java
@@ -5,5 +5,5 @@ import quickfix.Group;
 public class NoApplIDs extends Group {
 	private static final long serialVersionUID = 1L;
 	public static final int FIELD=1351;
-	public NoApplIDs() { super(FIELD,0); }
+	public NoApplIDs() { super(FIELD,0, new int[] { 1355, 1182, 1183 }); }
 }
