09dfb45fb0f4b9a0392ef7cb537fcd4c41bf50f5
wes@lasalletech.com <wes@lasalletech.com@43ba0a84-3b1b-f99a-8ad7-b8d2d923c440>
2011-08-24 20:11:04 +0000

    - Renamed ApplChannelID field to proper name 'RefApplID' - Added sequence order for NoApplIDs

--- trunk/java/src/main/java/com/lasalletech/umdf/decoder/fix_replay/fix_custom/RefApplID.java
+++ trunk/java/src/main/java/com/lasalletech/umdf/decoder/fix_replay/fix_custom/RefApplID.java
@@ -2,9 +2,9 @@ package com.lasalletech.umdf.decoder.fix_replay.fix_custom;
 
 import quickfix.StringField;
 
-public class ApplChannelID extends StringField {
+public class RefApplID extends StringField {
 	private static final long serialVersionUID = 1L;
 	public static final int FIELD=1355;
-	public ApplChannelID() { super(FIELD); }
-	public ApplChannelID(String in) { super(FIELD,in); }
+	public RefApplID() { super(FIELD); }
+	public RefApplID(String in) { super(FIELD,in); }
 }
