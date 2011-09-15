3026c276edadc69531ff1d37f27c413bbe07bd85
wes <wes@lasalletech.com>
2011-09-15 19:49:51 +0000

    Updated bvmf console app to have actual functionality

--- c++/src/bvmf/umdf_console/main.cc
+++ c++/src/bvmf/umdf_console/main.cc
@@ -4,7 +4,20 @@
   longdesc
 */
 
+#include <string>
+
+#include "session.h"
+
+using std::string;
+
 int main(int argc,char** argv) {
+
+  ifstream fast_template_file(argv[1]);
+
+  XMLTemplateParser parser;
+  TemplateRegistryPtr templates=parser.parse(fast_template_file);
+
+  BvmfSession session(market_data,templates);
 
   return 0;
 }
---
+++ c++/src/bvmf/umdf_console/session.h
@@ -0,0 +1,40 @@
+/** :mode=c++:indentSize=2:noTabs=true:tabSize=2:
+  @brief briefdesc
+
+  longdesc
+*/
+
+#ifndef SESSION_H_
+#define SESSION_H_ 1
+
+#include <boost/shared_ptr.hpp>
+
+#include <quickfast/Messages/MessageBuilder.h>
+#include <quickfast/Messages/Message.h>
+
+#include "umdf_decoder/message.h"
+#include "umdf_decoder/aggregator.h"
+
+class BvmfSession : public MessageBuilder {
+public:
+  BvmfSession::BvmfSession(
+      FastMarketDataManager& out,
+      TemplateRegistryPtr in_registry) :
+    target(out),registry(in_registry) {}
+
+  virtual void addField(
+    const QuickFast::Messages::FieldIdentityCPtr& identity,
+    const QuickFast::Messages::FieldCPtr& value) {
+    out_msg->addField(identity,value);
+  }
+
+  void on_recv_message(Message msg,Aggregator& source);
+
+private:
+  boost::shared_ptr<QuickFAST::Messages::Message> out_msg;
+  FastMarketDataManager& target;
+  TemplateRegistryPtr registry;
+};
+
+#endif // SESSION_H_
+
