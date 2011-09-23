3eaf2bdb4b8312ff03162120e9722fdb3362eb9a
wes <wes@lasalletech.com>
2011-09-23 18:17:26 +0000

    - fixed BvmfSession to work similarly to QuickFAST's SingleMessageConsumer

--- c++/src/bvmf/umdf_console/session.cc
+++ c++/src/bvmf/umdf_console/session.cc
@@ -6,8 +6,14 @@
 
 #include "session.h"
 
+#include <iostream>
+
 #include <quickfast/Codecs/Decoder.h>
 #include <quickfast/Codecs/DataSourceBuffer.h>
+
+using std::string;
+using std::cout;
+using std::endl;
 
 using QuickFAST::Codecs::Decoder;
 using QuickFAST::Codecs::Context;
@@ -19,14 +25,34 @@ void BvmfSession::on_recv_message(Message msg,Aggregator& source) {
   void* tmp_buf=new char[msg.total_size()];
   msg.read(tmp_buf,0,msg.total_size());
 
-  out_msg.reset(new QuickFAST::Messages::Message(0));
-
   Decoder decoder;
   GenericMessageBuilder builder(*this);
   decoder.decodeMessage(DataSourceBuffer(tmp_buf,out_msg.total_size()),builder);
 
   delete[] tmp_buf;
+}
+
+bool BvmfSession::consumeMessage(QuickFAST::Messages::Message& msg) {
+  out_msg.reset(new QuickFAST::Messages::Message(0));
+  out_msg->swap(msg);
 
   target.on_message(out_msg);
+
+  return true;
+}
+
+bool BvmfSession::logMessage(unsigned short,const string& msg) {
+  cout<<msg<<endl;
+  return true;
+}
+
+bool BvmfSession::reportDecodingError(const string& msg) {
+  cout<<msg<<endl;
+  return true;
+}
+
+bool BvmfSession::reportCommunicationError(const string& msg) {
+  cout<<msg<<endl;
+  return true;
 }
 
